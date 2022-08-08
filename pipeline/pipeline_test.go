f14a77ce2e06182437fabc6d508ec8ccd3ffd73e
zhangzhaojin40458 <zhangzhaojin40458@sangfor.com>
2022-08-08 10:13:46 +0800

    补充测试

--- pipeline/pipeline_test.go
+++ pipeline/pipeline_test.go
@@ -6,9 +6,10 @@ import (
 
 func Test_extractParams(t *testing.T) {
 	type args struct {
-		result []byte
-		e      ExtractParams
-		params map[string]string
+		result      []byte
+		e           ExtractParams
+		params      map[string]string
+		description string
 	}
 	tests := []struct {
 		name    string
@@ -37,10 +38,51 @@ func Test_extractParams(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "first test",
+			args: args{
+				result: []byte(
+					"{\"data\":{\"data\":null,\"count\":0,\"state\":\"running\"},\"msg\":null,\"code\":\"0000000\",\"success\":true}",
+				),
+				e: ExtractParams{
+					Type: "json",
+					Result: []ExtractResult{
+						{
+							Key:          "success",
+							Name:         "success",
+							Assert:       "true",
+							AssertResult: false,
+						},
+					},
+				},
+				params: map[string]string{},
+			},
+			wantErr: false,
+		}, {
+			name: "first test",
+			args: args{
+				result: []byte(
+					"{\"data\":{\"data\":null,\"count\":0,\"state\":\"running\"},\"msg\":null,\"code\":\"0000000\",\"success\":true}",
+				),
+				e: ExtractParams{
+					Type: "json",
+					Result: []ExtractResult{
+						{
+							Key:          "data.state",
+							Name:         "success",
+							Assert:       "running",
+							AssertResult: false,
+						},
+					},
+				},
+				params: map[string]string{},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := extractParams(tt.args.result, tt.args.e, tt.args.params); (err != nil) != tt.wantErr {
+			if err := extractParams(tt.args.result, tt.args.e, tt.args.params, tt.args.description); (err != nil) != tt.wantErr {
 				t.Errorf("extractParams() error = %v, wantErr %v, %+v\n", err, tt.wantErr, tt.args.e)
 			}
 		})
